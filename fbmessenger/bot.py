d4656874cf825f18b6feec0ce6c897f00d997ff3
jbblackett <masterblackett@gmail.com>
2016-12-12 20:31:39 +0000

    No work

---
+++ fbmessenger/bot.py
@@ -0,0 +1,8 @@
+from pymessenger.bot import Bot
+
+token = "EAANUWZBzhTh8BAPKIkJKZCjeo5R5oZBwQQbFpnVn0rfXazqvwCKuIGZBcUz76GxYwYUyFVOj8eUGsyxU55BEiWFvzl2W0qWJ3pOf6MwQwUprdPoSZAHQgbPW5GOgHsRArO925dGsWZBRHG0RLmopP12FIbOTYMKvHtFZA6hjD0GOAZDZD"
+
+bot = Bot(token)
+
+bot.send_text_message("JaydBlackett", "Hello")
+print("Done")
