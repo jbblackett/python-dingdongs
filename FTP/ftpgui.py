e78a5ad37ced0fe3159f85a8a8ce6a4ca6363d36
jbblackett <masterblackett@gmail.com>
2016-12-15 12:12:27 +0000

    Update ftpgui.py
    Added FTP command options and improved absolute positioning

--- FTP/ftpgui.py
+++ FTP/ftpgui.py
@@ -7,7 +7,6 @@ ftp = ftplib.FTP()
 def connectServer():
     ip = ent_ip.get()
     port = int(ent_port.get())
-    
     try:
         msg = ftp.connect(ip,port)
         text_servermsg.insert(END,"\n")
@@ -35,15 +34,97 @@ def loginServer():
 
         
 def displayDir():
+    libox_serverdir.insert(0,"----------------------")
     dirlist = []
     dirlist = ftp.nlst()
     for item in dirlist:
-        libox_serverdir.insert(END, item)
+        libox_serverdir.insert(0, item)
+
+#FTP commands
+def changeDirectory():
+    directory = ent_input.get()
+    try:
+        msg = ftp.cwd(directory)
+        text_servermsg.insert(END,"\n")
+        text_servermsg.insert(END,msg)
+    except:
+        text_servermsg.insert(END,"\n")
+        text_servermsg.insert(END,"Unable to change directory")
+    displayDir()
+
+def createDirectory():
+    directory = ent_input.get()
+    try:
+        msg = ftp.mkd(directory)
+        text_servermsg.insert(END,"\n")
+        text_servermsg.insert(END,msg)
+    except:
+        text_servermsg.insert(END,"\n")
+        text_servermsg.insert(END,"Unable to create directory")
+    displayDir()
+    
+def deleteDirectory():
+    directory = ent_input.get()
+    try:
+        msg = ftp.rmd(directory)
+        text_servermsg.insert(END,"\n")
+        text_servermsg.insert(END,msg)
+    except:
+        text_servermsg.insert(END,"\n")
+        text_servermsg.insert(END,"Unable to delete directory")
+    displayDir()
+    
+def deleteFile():
+    file = ent_input.get()
+    try:
+        msg = ftp.delete(file)
+        text_servermsg.insert(END,"\n")
+        text_servermsg.insert(END,msg)
+    except:
+        text_servermsg.insert(END,"\n")
+        text_servermsg.insert(END,"Unable to delete file")
+    displayDir()
+    
+def downloadFile():
+    file = ent_input.get()
+    down = open(file, "wb")
+    try:
+        text_servermsg.insert(END,"\n")
+        text_servermsg.insert(END,"Downloading " + file + "...")
+        text_servermsg.insert(END,"\n")
+        text_servermsg.insert(END,ftp.retrbinary("RETR " + file, down.write))
+    except:
+        text_servermsg.insert(END,"\n")
+        text_servermsg.insert(END,"Unable to download file")
+    displayDir()
+    
+def uploadFile():
+    file = ent_input.get()
+    try:
+        up = open(file, "rb")
+        text_servermsg.insert(END,"\n")
+        text_servermsg.insert(END,"Uploading " + file + "...")
+        text_servermsg.insert(END,"\n")
+        text_servermsg.insert(END,ftp.storbinary("STOR " + file,up))
+    except:
+        text_servermsg.insert(END,"\n")
+        text_servermsg.insert(END,"Unable to upload file")
+    displayDir()
+    
+def closeConnection():
+    try:
+        text_servermsg.insert(END,"\n")
+        text_servermsg.insert(END,"Closing connection...")
+        text_servermsg.insert(END,"\n")
+        text_servermsg.insert(END,ftp.quit())
+    except:
+        text_servermsg.insert(END,"\n")
+        text_servermsg.insert(END,"Unable to disconnect")
 
 window = tkinter.Tk()
 window.title("FTP Client")
 window.wm_iconbitmap("favicon.ico")
-window.geometry("1280x1084")
+window.geometry("1000x600")
 
 #Connect
 lbl_ip = tkinter.Label(window, text="IP Address")
@@ -67,7 +148,15 @@ lbl_dir = tkinter.Label(window, text="Directory listing:")
 libox_serverdir = tkinter.Listbox(window)
 
 #Options
-
+lbl_input = tkinter.Label(window, text="Input")
+ent_input = tkinter.Entry(window)
+btn_chdir = tkinter.Button(window, text="Change Directory", command=changeDirectory)
+btn_crdir = tkinter.Button(window, text="Create Directory", command=createDirectory)
+btn_deldir = tkinter.Button(window, text="Delete Directory", command=deleteDirectory)
+btn_delfile = tkinter.Button(window, text="Delete File", command=deleteFile)
+btn_downfile = tkinter.Button(window, text="Download File", command=downloadFile)
+btn_upfile = tkinter.Button(window, text="Upload File", command=uploadFile)
+btn_quit = tkinter.Button(window, text="Disconnect", command=closeConnection)
 
 #Place widgits
 lbl_ip.place(x=20,y=20)
@@ -76,14 +165,34 @@ lbl_port.place(x=20,y=60)
 ent_port.place(x=20,y=80)
 btn_connect.place(x=52,y=110)
 text_servermsg.place(x=20,y=150)
-lbl_login.place(x=20,y=550)
-ent_login.place(x=20,y=570)
-lbl_pass.place(x=20,y=590)
-ent_pass.place(x=20,y=610)
-btn_login.place(x=52,y=640)
+lbl_login.place(x=150,y=20)
+ent_login.place(x=150,y=40)
+lbl_pass.place(x=150,y=60)
+ent_pass.place(x=150,y=80)
+btn_login.place(x=150,y=110)
 
 lbl_dir.place(x=700,y=143)
 libox_serverdir.place(x=700,y=165)
+
+lbl_input.place(x=700,y=400)
+ent_input.place(x=700,y=420)
+btn_chdir.place(x=700,y=450)
+btn_crdir.place(x=700,y=480)
+btn_deldir.place(x=700,y=510)
+btn_delfile.place(x=700,y=540)
+
+btn_downfile.place(x=850,y=450)
+btn_upfile.place(x=850,y=480)
+btn_quit.place(x=850,y=510)
+
+libox_serverdir.config(height=14,width=40)
+btn_chdir.config(width=15)
+btn_crdir.config(width=15)
+btn_deldir.config(width=15)
+btn_delfile.config(width=15)
+btn_downfile.config(width=15)
+btn_upfile.config(width=15)
+btn_quit.config(width=15)
 
 #Create
 window.mainloop()
