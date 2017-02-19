cecf3db60ede56b5da4e4fdb462e6ef255a6b217
jbblackett <masterblackett@gmail.com>
2017-02-19 19:47:59 +0000

    New working

--- cleverbot/cleverbotXcleverbot.py
+++ cleverbot/cleverbotXcleverbot.py
@@ -1,12 +1,50 @@
-import cleverbot
+import requests
+import urllib
 
-cb1 = cleverbot.Cleverbot()
-cb2 = cleverbot.Cleverbot()
+class Cleverbot(object):
+    """Handles a conversation with Cleverbot.
+    Example usage:
+       >>> from cleverbot import Cleverbot
+       >>> cb = Cleverbot('my-api-key')
+       >>> cb.ask("Hi. How are you?")
+       "Hello"
 
-text = cb1.ask("Hi")
-print("Hello")
+    http://www.cleverbot.com/apis
+    """
+    def __init__(self, key='adaf89e6a04492eafbe3277bf6514269', q='Hello'):
+        self.HOST = "www.cleverbot.com"
+
+        self.PROTOCOL = "https://"
+        self.RESOURCE = "/getreply"
+
+        self.key = key
+        self.inital_q = urllib.parse.quote_plus(q)
+
+        self.SERVICE_URL = self.PROTOCOL + self.HOST + self.RESOURCE + "?key=" + self.key + "&input=" + self.inital_q
+        r = requests.get(self.SERVICE_URL)
+        content = r.json()
+        self.cs = content['cs']
+        self.conversation_id = content['conversation_id']
+        self.session = requests.Session()
+        self.conversation = []
+
+    def ask(self, q):
+        self.conversation.append(q)
+        question = urllib.parse.quote_plus(q.encode('utf8'))
+        url = self.SERVICE_URL + "&input=" + question + "&cs=" + self.cs
+
+        r = requests.get(url).json()
+        self.conversation.append(r['output'])
+        return r['output']
+
+c1 = Cleverbot()
+c2 = Cleverbot()
+
+msg = 'hi'
+response = c1.ask(msg)
+
 while True:
-    text = cb2.ask(text)
-    print("Cleverbot 2: " + text)
-    text = cb1.ask(text)
-    print("Cleverbot 1: " + text)
+    print('Cleverbot 1: ' + response)
+    response = c2.ask(response)
+    print('Cleverbot 2: ' + response)
+    response = c1.ask(response)
